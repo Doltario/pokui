dfca2589976d44eadb71514b12a66a7713c1efbe
Félix Darraud <felix.darraud@gmail.com>
2021-01-22 19:23:44 +0100

    Added navbar style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'pokui-navbar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  faCog = faCog;
 
   constructor() { }
 
